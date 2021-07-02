393fe537460af8283b6fea53eb6271a08dada12b
Harley Bekker <harleybekker@outlook.com>
2021-07-02 16:48:12 +1000

    Set up the Input System and started on the player gameobject

---
+++ Narcomancer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    Vector2 moveInput;
+    Vector2 lookInput;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+    }
+
+    #region Input handling
+
+    public void OnMove(InputAction.CallbackContext value)
+    {
+        Vector2 input = value.ReadValue<Vector2>();
+        moveInput = input;
+    }
+    public void OnLook(InputAction.CallbackContext value)
+    {
+        Vector2 input = value.ReadValue<Vector2>();
+        lookInput = input;
+    }
+
+    #endregion
+
+
+    #region Device connections
+    public void OnDeviceLost()
+    {
+
+    }
+    public void OnDeviceRegained()
+    {
+
+    }
+    public void OnControlsChanged()
+    {
+
+    }
+    #endregion
+}
