23a0bb87e6dcf8b2ca8a4457e5c0c850079277f8
Harley Bekker <harleybekker@outlook.com>
2021-08-12 09:46:25 +1000

    Created: Destructible object script

---
+++ Narcomancer/Assets/Scripts/DestructibleObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructibleObject : MonoBehaviour
+{
+    public float health = 10f;
+    public float maxHealth = 10f;
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+
+        if (health <= 0f)
+            Destroy(gameObject);
+    }
+
+    public void Heal(float healAmount)
+    {
+        health += healAmount;
+
+        if (health > maxHealth)
+            health = maxHealth;
+    }
+}
