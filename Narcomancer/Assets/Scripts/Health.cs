a2413deb42f9d183f806fcc6f605685999e739c9
Harley Bekker <harleybekker@outlook.com>
2021-09-16 20:35:01 +1000

    Added: ui for the health, shotgun ammo and neon ammo

--- Narcomancer/Assets/Scripts/Health.cs
+++ Narcomancer/Assets/Scripts/Health.cs
@@ -4,27 +4,27 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-    public float m_Health = 100;
+    public float m_CurrentHealth = 100;
     public float m_MaxHealth = 100;
 
     public bool m_IsDead = false;
 
     public void Damage(float damage)
     {
-        m_Health -= damage;
+        m_CurrentHealth -= damage;
 
-        if (m_Health <= 0)
+        if (m_CurrentHealth <= 0)
         {
-            m_Health = 0;
+            m_CurrentHealth = 0;
             m_IsDead = true;
         }
     }
 
     public void Heal(float healAmount)
     {
-        m_Health += healAmount;
+        m_CurrentHealth += healAmount;
 
-        if (m_Health > m_MaxHealth)
-            m_Health = m_MaxHealth;
+        if (m_CurrentHealth > m_MaxHealth)
+            m_CurrentHealth = m_MaxHealth;
     }
 }
