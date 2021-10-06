cbdde9bad9df71745a59b9aee1111e50753bad75
Ryan Lovell <ryanlovell96@gmail.com>
2021-10-06 14:51:06 +1100

    Designer Code day 1

---
+++ Narcomancer/Assets/Scripts/Designer Code/Door.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+   public GameObject player;
+   DoorAnimation doorAnim;
+   Animator playerAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerAnim = player.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    public void Run()
+    {
+        //doorAnim;
+       playerAnim.SetTrigger("doorkick");
+       doorAnim.OpenDoors();
+        
+    }
+}
---
+++ Narcomancer/Assets/Scripts/Designer Code/InteractionMasterScript.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionMasterScript : MonoBehaviour
+{
+    [System.Serializable]
+    public enum EventTriggerName {Door, Money, Powerbox, Drugs};
+    public EventTriggerName eventName;
+    public GameObject switchObject;
+
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+
+    public void Interact()
+    {
+        switch (eventName)
+        {
+            case EventTriggerName.Door:
+                print("Player kicks open door");
+                gameObject.SendMessage("Run");
+                break;
+            case EventTriggerName.Money:
+                print("Player takes the money");
+                gameObject.SetActive(false);
+                break;
+            case EventTriggerName.Powerbox:
+                print("Player turns off the power");
+                switchObject.SetActive(true);
+                gameObject.SetActive(false);
+                break;
+            case EventTriggerName.Drugs:
+                print("Player destorys the drugs");
+                switchObject.SetActive(true);
+                gameObject.SetActive(false);
+                break;
+
+
+        }
+    }
+
+}
