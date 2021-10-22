ea52b7ea6a7e76791e302b10dd2c582b3b1b7437
Ryan Lovell <ryanlovell96@gmail.com>
2021-10-22 15:59:24 +1100

    Monster Closets set up

--- Narcomancer/Assets/Scripts/Designer Code/MonsterCloset.cs
+++ Narcomancer/Assets/Scripts/Designer Code/MonsterCloset.cs
@@ -4,35 +4,60 @@ using UnityEngine;
 
 public class MonsterCloset : MonoBehaviour
 {
-    private bool dooropen;  
+    public bool dooropen;
     public GameObject door;
     Animator anim;
+    public int counter;
     // Start is called before the first frame update
     void Start()
     {
        anim = door.GetComponent<Animator>();
+        counter = 0;   
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (dooropen) { anim.Play("Door Opening"); print("dooropen"); }
-        if (!dooropen) { anim.Play("Door Close"); print("doonclose"); }
+        if (counter <= 0) { dooropen = false; }
+        if (counter >0) { dooropen = true; }
+        if (dooropen) { anim.SetBool("dooropen", true); }//print("dooropen"); }
+        if (!dooropen) { anim.SetBool("dooropen", false); }// print("doonclose"); }
     }
 
-    private void OnTriggerStay(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player")
+        if (other.tag == "Enemy")
         {
-            dooropen = true;
-        }
-        else
-        {
-            dooropen = false;
+            counter++;
         }
     }
- 
 
-   
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            counter--;
+        }
+    }
+    // private void OnTriggerStay(Collider other)
+    // {
+    //   if (other.tag == "Player")
+    // {
+    //   nobodyhome = false;
+    // dooropen = true;
+
+    // }
+
+
+    //private void OnTriggerExit(Collider other)
+    //{
+    //  if (other.tag == "Player")
+    // {
+    //    nobodyhome = true;
+    //  print("playerleft");
+    //  }
+    //}
+
+
 }
 
