0a15cd08b5a2ed9b9ac0fc103cead51b9723bb20
Baz <bazlever6@gmail.com>
2021-10-22 17:03:05 +1100

    Options menu

--- Narcomancer/Assets/Scripts/MainMenu.cs
+++ Narcomancer/Assets/Scripts/MainMenu.cs
@@ -7,6 +7,19 @@ public class MainMenu : MonoBehaviour
 {
 
     int sceneToLoad;
+    public Animator optionsAnim;
+    bool settingsOpen = false;
+
+    float deltaTimer;
+    float deltaTimer1;
+
+    private void Update()
+    {
+        if (deltaTimer1 <= deltaTimer)
+        {
+            deltaTimer1 += Time.deltaTime;
+        }
+    }
 
     public void OnClickPlay()
     {
@@ -17,6 +30,23 @@ public class MainMenu : MonoBehaviour
     public void OnClickOptions()
     {
         Debug.Log("Opening Options Menu");
+
+        if (deltaTimer1 > deltaTimer)
+        {
+            if (settingsOpen == false)
+            {
+                optionsAnim.SetBool("SettingsOpen", true);
+                settingsOpen = true;
+            }
+            else
+            {
+                optionsAnim.SetBool("SettingsOpen", false);
+                settingsOpen = false;
+            }
+        }
+        deltaTimer1 = 0f;
+
+
     }
 
     public void OnClickCredits()
