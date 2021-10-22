a2149c5e903ac12c264a4961f60ffa8f7cfa9e14
Ryan Lovell <ryanlovell96@gmail.com>
2021-10-22 14:09:28 +1100

    Monster Closets and Music

---
+++ Narcomancer/Assets/Scripts/Designer Code/MonsterCloset.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterCloset : MonoBehaviour
+{
+    private bool dooropen;  
+    public GameObject door;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+       anim = door.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dooropen) { anim.Play("Door Opening"); print("dooropen"); }
+        if (!dooropen) { anim.Play("Door Close"); print("doonclose"); }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            dooropen = true;
+        }
+        else
+        {
+            dooropen = false;
+        }
+    }
+ 
+
+   
+}
+
