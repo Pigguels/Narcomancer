0db0fd631bda90f48ff6e9556e478b3cac74594f
Harry Fuge <harry.fuge@gmail.com>
2021-11-05 11:41:43 +1100

    NavMesh changes and beginings of window force

---
+++ Narcomancer/Assets/Scripts/WindowBreak.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowBreak : MonoBehaviour
+{
+    public Rigidbody[] shards;
+    
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        foreach (Rigidbody rb in shards)
+        {
+            rb.AddForce(collision.impulse, ForceMode.Impulse);
+        }
+    }
+
+
+}
