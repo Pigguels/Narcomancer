6cbde9bfec67e10be4917e7a43157861ee05bfc4
Harry Fuge <harry.fuge@gmail.com>
2021-11-17 17:27:21 +1100

    Updated the Navmesh and the bar's had colliders that were perpendicular.

---
+++ Narcomancer/Assets/Scripts/GrenadeTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrenadeTrigger : MonoBehaviour
+{
+    public GameObject explosion;
+    public float grenadeLifetime = 3f;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "IgnoreGrenade" || collision.gameObject.name == "Narcomancer_v03")
+            return;
+        else
+        {
+            GameObject test = Instantiate(explosion, transform.position, transform.rotation);
+            Destroy(test, grenadeLifetime);
+            Destroy(gameObject);
+        }
+        
+    }
+}
